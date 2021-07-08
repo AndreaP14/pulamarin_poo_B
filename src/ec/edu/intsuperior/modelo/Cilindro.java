c3a6d9275441e5b546ab5e601719256ff470bb56
user <user@LAPTOP-5UGCIOTV>
2021-07-07 19:09:58 -0500

    Primera version Figuras Geometricas 2DB "B"

---
+++ src/ec/edu/intsuperior/modelo/Cilindro.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.modelo;
+
+/**
+ *
+ * @author user
+ */
+public class Cilindro {
+    private String circulo;
+    private String linea;
+    
+    
+}
---
+++ src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.vista;
+
+import ec.edu.intsuperior.modelo.Punto;
+import ec.edu.intsuperior.modelo.Circulo;
+import java.util.Scanner;
+/**
+ *
+ * @author user
+ */
+public class Aplicacion {
+    public static void main (String []args){
+        Scanner input = new Scanner(System.in);
+        
+        Punto p1 = new Punto (5,7);
+        Punto p2 = new Punto ();
+        System.out.println("Ingrese el valor del punto 2:");
+        p2.setx(input.nextInt());
+        p2.setY(input.nextInt());
+        
+        System.out.println("La distancia entre los puntos son:"+
+                p1.distanciaEntreDosPuntos(p1.getX(),p2.getX(),p1.getY(),p2.getY()));
+        
+        Circulo c1 = new Circulo(5, p1);
+        System.out.println("El area de circulo es:" +c1.areaCirculo());
+    }
+}
